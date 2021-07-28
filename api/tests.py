d0ba740bb87d54d3b7d6fd80055b1c2f946afcc6
유병민 <lims@yubyeongmin-ui-MacBookPro.local>
2021-07-29 00:30:43 +0900

    Fix jenkinsfile

--- api/tests.py
+++ api/tests.py
@@ -2,7 +2,7 @@ from accounts.models import User
 
 #from rest_framework                  import status
 #from rest_framework.authtoken.models import Token
-#from rest_framework.test             import APITestCase
+from rest_framework.test             import APITestCase
 #
 #from .models import Payment
 
